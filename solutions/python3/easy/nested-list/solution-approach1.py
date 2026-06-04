996a5f12cbc5e9d520b12421ad9b28c30d8e29ea
Muhammad Arya Putra Handrian <aryaputraa1911@gmail.com>
2026-06-04 17:49:21 +0700

    ✅ Nested Lists [python3] — approach1

---
+++ solutions/python3/easy/nested-list/solution-approach1.py
@@ -0,0 +1,26 @@
+# ──────────────────────────────────────────────────
+# Problem     Nested Lists
+# Difficulty  Easy
+# Subdomain   Basic Data Types
+# Platform    HackerRank
+# Language    python3
+# Status      Accepted
+# Submitted   2026-06-04, 05:49 p.m.
+# ──────────────────────────────────────────────────
+
+if __name__ == '__main__':
+    all_data = []
+    for _ in range(int(input())):
+        name = input()
+        score = float(input())
+        student = list((name,score))
+        all_data.append([name,score])
+        
+score = [i[1] for i in all_data]
+unique = sorted(set(score))
+result = unique[1]
+
+name = [i[0] for i in all_data if i[1]==result]
+name.sort()
+for i in name:
+    print(i)    
