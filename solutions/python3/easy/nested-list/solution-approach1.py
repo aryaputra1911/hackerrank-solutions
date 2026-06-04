0d0562d638f174690c3a4bb5c923f8cc599aa843
Muhammad Arya Putra Handrian <aryaputraa1911@gmail.com>
2026-06-04 19:50:37 +0700

    ✅ Nested Lists [python3] — approach1

--- solutions/python3/easy/nested-list/solution-approach1.py
+++ solutions/python3/easy/nested-list/solution-approach1.py
@@ -5,7 +5,7 @@
 # Platform    HackerRank
 # Language    python3
 # Status      Accepted
-# Submitted   2026-06-04, 07:49 p.m.
+# Submitted   2026-06-04, 07:50 p.m.
 # ──────────────────────────────────────────────────
 
 if __name__ == '__main__':
