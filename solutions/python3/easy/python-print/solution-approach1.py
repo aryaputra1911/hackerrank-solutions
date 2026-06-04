ba9b9ce08bc6a1128beff50b2424c02d934c0b82
Muhammad Arya Putra Handrian <aryaputraa1911@gmail.com>
2026-06-04 10:35:28 +0700

    ✅ Print Function [python3] — approach1

---
+++ solutions/python3/easy/python-print/solution-approach1.py
@@ -0,0 +1,18 @@
+# ──────────────────────────────────────────────────
+# Problem     Print Function
+# Difficulty  Easy
+# Subdomain   Introduction
+# Platform    HackerRank
+# Language    python3
+# Status      Accepted
+# Submitted   2026-06-04, 10:35 a.m.
+# ──────────────────────────────────────────────────
+
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(n+1):
+        if i > 0:
+            print(i,end="")
+        
+
+    
