b97b11ec9383b25e908c993efb71a35456979d72
Muhammad Arya Putra Handrian <aryaputraa1911@gmail.com>
2026-06-04 15:39:39 +0700

    ✅ Find the Runner-Up Score!   [python3] — approach1

---
+++ solutions/python3/easy/find-second-maximum-number-in-a-list/solution-approach1.py
@@ -0,0 +1,15 @@
+# ──────────────────────────────────────────────────
+# Problem     Find the Runner-Up Score!  
+# Difficulty  Easy
+# Subdomain   Basic Data Types
+# Platform    HackerRank
+# Language    python3
+# Status      Accepted
+# Submitted   2026-06-04, 03:39 p.m.
+# ──────────────────────────────────────────────────
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = map(int, input().split())
+    num = sorted(list(set(arr)))
+    print(num[-2])
