653d11d324b91fe5184d4c35b18be513865b82c1
Muhammad Arya Putra Handrian <aryaputraa1911@gmail.com>
2026-06-10 22:42:00 +0700

    ✅ Merge the Tools! [python3] — approach1

---
+++ solutions/python3/medium/merge-the-tools/solution-approach1.py
@@ -0,0 +1,17 @@
+# ──────────────────────────────────────────────────
+# Problem     Merge the Tools!
+# Difficulty  Medium
+# Subdomain   Strings
+# Platform    HackerRank
+# Language    python3
+# Status      Accepted
+# Submitted   2026-06-10, 10:41 p.m.
+# ──────────────────────────────────────────────────
+
+def merge_the_tools(string, k):
+    # your code goes here
+    chunks = [string[i:i + k]for i in range(0, len(string), k)]
+    for i in chunks:
+        unique_letters = list(dict.fromkeys(i))
+        results = "".join(unique_letters)
+        print(results)
