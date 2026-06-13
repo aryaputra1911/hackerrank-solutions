be247cfc2e2facb28b235b4129c900b7c1ad85fb
Muhammad Arya Putra Handrian <aryaputraa1911@gmail.com>
2026-06-13 07:54:48 +0700

    ✅ Arrays - DS [python3] — approach1

---
+++ solutions/python3/easy/arrays-ds/solution-approach1.py
@@ -0,0 +1,34 @@
+# ──────────────────────────────────────────────────
+# Problem     Arrays - DS
+# Difficulty  Easy
+# Subdomain   Arrays
+# Platform    HackerRank
+# Language    python3
+# Status      Accepted
+# Submitted   2026-06-13, 07:54 a.m.
+# ──────────────────────────────────────────────────
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+def reverseArray(a):
+    return a[::-1]
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr_count = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    res = reverseArray(arr)
+
+    fptr.write(' '.join(map(str, res)))
+    fptr.write('\n')
+
+    fptr.close()
