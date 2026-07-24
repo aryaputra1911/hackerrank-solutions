39c8504cca6971efc9a3a3b10e4621e814117247
Muhammad Arya Putra Handrian <aryaputraa1911@gmail.com>
2026-07-24 16:09:23 +0700

    ✅ Functions [cpp] — approach1

---
+++ solutions/cpp/easy/c-tutorial-functions/solution-approach1.cpp
@@ -0,0 +1,37 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/challenges/c-tutorial-functions/problem?isFullScreen=true
+// Problem     Functions
+// Difficulty  Easy
+// Subdomain   Introduction
+// Platform    HackerRank
+// Language    cpp
+// Status      Accepted
+// Submitted   2026-07-24, 04:09 p.m.
+// ──────────────────────────────────────────────────
+
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+/*
+Add `int max_of_four(int a, int b, int c, int d)` here.
+*/
+int max_of_four(int a, int b, int c, int d){
+    int max = a;
+       if(b>max) max = b;
+       if(c>max) max = c;
+       if(d>max) max = d;
+
+    return max;
+}
+
+int main() {
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int ans = max_of_four(a, b, c, d);
+    printf("%d", ans);
+    
+    
+    
+    return 0;
+}
