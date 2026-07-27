8ab59b4d56d6dcca4c2fc8e5c9ad69fe95667fdb
Muhammad Arya Putra Handrian <aryaputraa1911@gmail.com>
2026-07-27 18:44:33 +0700

    ✅ Arrays Introduction [cpp] — approach2

---
+++ solutions/cpp/easy/arrays-introduction/solution-approach2.cpp
@@ -0,0 +1,35 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/challenges/arrays-introduction/problem?isFullScreen=true
+// Problem     Arrays Introduction
+// Difficulty  Easy
+// Subdomain   Introduction
+// Platform    HackerRank
+// Language    cpp
+// Status      Accepted
+// Submitted   2026-07-27, 06:44 p.m.
+// ──────────────────────────────────────────────────
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int n;
+    int arr[10000];
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+    std :: reverse(arr, arr+n);
+    
+    for(int i=0; i < n ; i++){
+        cout<<arr[i]<<" ";
+    }
+    
+    return 0;
+}
